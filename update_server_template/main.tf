0824b960513a583f21961c17cf214e0b8ddce2c1
scotttyso <tyscott@cisco.com>
2024-02-17 22:50:27 -0500

    updated and removed bandwidth_percent

--- update_server_template/main.tf
+++ update_server_template/main.tf
@@ -42,13 +42,13 @@ locals {
         ) > 0 ? local.template_moids["${org}/${v.ucs_server_template}"] : "UNUSED"
         name            = e.name
         organization    = org
-        resource_pool = lookup(v, "resource_pool", "UNUSED")
-        serial_number = lookup(e, "serial_number", "unknown")
+        resource_pool   = lookup(v, "resource_pool", "UNUSED")
+        serial_number   = lookup(e, "serial_number", "unknown")
         target_platform = lookup(v, "target_platform", "FIAttached")
         tmoid = length(regexall("UNUSED", lookup(data.terraform_remote_state.map.outputs.profiles.map.server["${org}/${e.name}"], "ucs_server_template", "UNUSED"))
         ) == 0 ? local.template_moids[data.terraform_remote_state.map.outputs.profiles.map.server["${org}/${e.name}"].ucs_server_template] : "UNUSED"
         ucs_server_template = length(regexall("/", lookup(v, "ucs_server_template", "UNUSED"))
-          ) > 0 ? v.ucs_server_template : length(compact([lookup(v, "ucs_server_template", "")])) > 0 ? "${org}/${v.ucs_server_template}" : ""
+        ) > 0 ? v.ucs_server_template : length(compact([lookup(v, "ucs_server_template", "")])) > 0 ? "${org}/${v.ucs_server_template}" : ""
         uuid_pool = length(regexall("/", lookup(v, "ucs_server_template", "UNUSED"))
           ) > 0 ? lookup(data.terraform_remote_state.map.outputs.profiles.map.template[v.ucs_server_template], "uuid_pool", "UNUSED"
           ) : length(compact([lookup(v, "ucs_server_template", "")])
@@ -86,34 +86,34 @@ data "utils_yaml_merge" "model" {
 }
 
 resource "intersight_server_profile" "detach" {
-  for_each = { for k, v in local.server : k => v if v.attach_template == true && v.new_moid != v.tmoid }
-  additional_properties = jsonencode({SrcTemplate = null})
-  name     = each.value.name
+  for_each              = { for k, v in local.server : k => v if v.attach_template == true && v.new_moid != v.tmoid }
+  additional_properties = jsonencode({ SrcTemplate = null })
+  name                  = each.value.name
   lifecycle { ignore_changes = all }
   organization { moid = data.terraform_remote_state.map.outputs.orgs[each.value.organization] }
   server_assignment_mode = length(regexall("UNUSED", each.value.resource_pool)) == 0 ? "Pool" : length(regexall(
     "^[A-Z]{3}[2-3][\\d]([0][1-9]|[1-4][0-9]|[5][0-3])[\\dA-Z]{4}$", each.value.serial_number)
   ) > 0 ? "Static" : "None"
   uuid_address_type = each.value.uuid_pool != "UNUSED" ? "POOL" : "NONE"
-  target_platform = each.value.target_platform
+  target_platform   = each.value.target_platform
 }
 
-#resource "intersight_bulk_mo_merger" "trigger_update" {
-#  for_each     = local.templates
-#  merge_action = "Merge"
-#  lifecycle { ignore_changes = all }
-#  sources {
-#    object_type = "server.ProfileTemplate"
-#    moid        = data.terraform_remote_state.map.outputs.profiles.map.template[each.key].moid
-#  }
-#  dynamic "targets" {
-#    for_each = { for v in each.value.servers : v => v }
-#    content {
-#      object_type = "server.Profile"
-#      moid        = data.terraform_remote_state.map.outputs.profiles.map.server[targets.key].moid
-#    }
-#  }
-#}
+resource "intersight_bulk_mo_merger" "trigger_update" {
+  for_each     = local.templates
+  merge_action = "Merge"
+  lifecycle { ignore_changes = all }
+  sources {
+    object_type = "server.ProfileTemplate"
+    moid        = data.terraform_remote_state.map.outputs.profiles.map.template[each.key].moid
+  }
+  dynamic "targets" {
+    for_each = { for v in each.value.servers : v => v }
+    content {
+      object_type = "server.Profile"
+      moid        = data.terraform_remote_state.map.outputs.profiles.map.server[targets.key].moid
+    }
+  }
+}
 output "server" {
   value = local.server
   #value = { for k, v in lookup(lookup(lookup(lookup(data.terraform_remote_state.map, "outputs", {}
