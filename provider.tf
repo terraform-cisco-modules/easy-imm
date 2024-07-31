ca788a58dcd1b16ffc15598be697221861493170
scotttyso <tyscott@cisco.com>
2024-07-31 09:27:29 -0400

    updates after deep testing on ethernet_adapter

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = "1.0.50"
+      version = "1.0.47"
     }
     time = {
       source  = "time"
